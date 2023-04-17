78826d88502a38c5393806d183f0226c1f5ce911
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-04-17 14:12:34 -0300

    :sparkles: Feat: Creating the search_by_title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,24 @@
+from tech_news.database import search_news
+import re
+
+
+def ignore_sensitive_case(title):
+    return re.compile(title, re.I)
+
+
+def create_formatted_news_list(list_news_by_title):
+    news_list = []
+    for news in list_news_by_title:
+        news = (news["title"], news["url"])
+        news_list.append(news)
+    return news_list
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    modificated_title = ignore_sensitive_case(title)
+    get_news_by_title = search_news({"title": modificated_title})
+    return create_formatted_news_list(get_news_by_title)
 
 
 # Requisito 8
