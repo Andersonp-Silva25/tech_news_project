c14e8dfdba759535c1ca9ecfaeaecd2b91194a61
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-04-18 23:12:06 -0300

    :sparkles: Feat: Creating the top_5_categories function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,23 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
+def get_categories(list_news):
+    all_categories = []
+    for new in list_news:
+        all_categories.append(new["category"])
+    return sorted(all_categories)
+
+
+def most_common_counter(list, limit):
+    counter = Counter(list)
+    return counter.most_common(limit)
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    list_news = find_news()
+    all_categories = get_categories(list_news)
+    most_common_categories = most_common_counter(all_categories, 5)
+    top_5_list = [category[0] for category in most_common_categories]
+    return top_5_list
